c1d7c0ed6daf48c5a225a83ed0ca891b3748404a
Risbel Suarez <risbel961019@gmail.com>
2024-04-30 09:45:58 -0400

    feat: Add CORS headers

--- src/index.ts
+++ src/index.ts
@@ -50,10 +50,38 @@ const { originAllowedDev, originAllowedPro }: any = config;
 const app = express();
 var bodyParser = require("body-parser");
 
+const allowedOrigins = [originAllowedPro, originAllowedDev];
+
 app.use(
   cors({
-    origin: [originAllowedPro, originAllowedDev],
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg = "The CORS policy for this site does not " + "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+      "Cookie",
+      "Accept",
+      "Accept-Language",
+      "User-Agent",
+      "Referer",
+      "Cache-Control",
+      "Pragma",
+      "Host",
+      "Connection",
+      "Content-Length",
+    ],
   })
 );
 
